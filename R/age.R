d1273268710f6108344d748f4e4c0a0d9c93018f
Hu <kevin.hu@bccdc.ca>
2023-06-29 14:53:33 -0700

    improved age functions with warnings

--- R/age.R
+++ R/age.R
@@ -1,12 +1,23 @@
-compute_age <- function(bday, eday, trans = FALSE, .transfn = lubridate::ymd) {
-  if (trans) lubridate::interval(.transfn(bday), .transfn(eday)) / lubridate::years(1)
-  else lubridate::interval(bday, eday) / lubridate::years(1)
+compute_age <- function(bday, eday, trans = FALSE, .transfn = lubridate::ymd, verbose = TRUE) {
+  if (trans) age <- lubridate::interval(.transfn(bday), .transfn(eday)) / lubridate::years(1)
+  else age <- lubridate::interval(bday, eday) / lubridate::years(1)
+  if (verbose)
+  {
+    if(is.na(min(age))) warning("Age contains missing value(s)")
+    if(min(age, na.rm = TRUE) < 0) warning("Age contains negative value(s)")
+    if(max(age, na.rm = TRUE) > 100) warning("Age contains value(s) > 100")
+  }
+  return(age)
 }
 
-cut_age <- function(age, lower_brks = c(0, 19, 25, 35, 45, 55), ...) {
+cut_age <- function(age, lower_brks = c(0, 19, 25, 35, 45, 55), verbose = TRUE, ...) {
   labs <- sapply(1:length(lower_brks),
          function(i) dplyr::case_when(i == 1 ~ paste0("<", lower_brks[i+1]),
                                i == length(lower_brks) ~ paste0(lower_brks[i], "+"),
                                .default = paste(lower_brks[i], lower_brks[i+1]-1, sep = "-")))
-  cut(age, breaks = c(lower_brks, Inf), labels = labs, right = FALSE, ...)
+  agegrp <- cut(age, breaks = c(lower_brks, Inf), labels = labs, right = FALSE, ...)
+  if (verbose) {
+    if (sum(is.na(agegrp)) > sum(is.na(age))) warning("More NA than raw age. Breaks do not cover the full range.")
+  }
+  return(agegrp)
 }
--- R/identify_code.R
+++ R/identify_code.R
@@ -1,18 +1,39 @@
 identify_code <- function(dat, source_nm, diag_cd_prefix, start_dt_nm, end_dt_nm = NULL, person_id_nm = "moh_study_id",
-                          codes, age_range, date_range, location_filter) {
+                          codes, bd_source = NULL, bd_col_nm = NULL, .dt_transfn = NULL) {
+
+  #get age
+  if (!is.null(bd_source))
+  {
+    #loop over the list to apply dplyr::filter for each source
+    dat <- dat %>% dplyr::left_join(bd_source %>% dplyr::select({{person_id_nm}}, {{bd_col_nm}}), by = person_id_nm)
+  }
+
   #prep data unifying variable names
 
   dat <- dat %>%
     dplyr::rename(dplyr::any_of(
       c(source_id = paste0(source_nm, "_id"),
-      person_id = person_id_nm,
-      start_dt = start_dt_nm,
-      end_dt = end_dt_nm)
-      )) %>%
+        person_id = person_id_nm,
+        start_dt = start_dt_nm,
+        end_dt = end_dt_nm,
+        b_dt = bd_col_nm)
+    )) %>%
     #remove empty diag columns
     dplyr::select(dplyr::where(~!all(is.na(.)))) %>%
     dplyr::rename_with(.fn = ~ gsub(diag_cd_prefix, "diag_cd", .), .col = dplyr::starts_with(diag_cd_prefix)) %>%
     dplyr::mutate(source = source_nm)
+
+  #transform dates if needed
+  if (!is.null(.dt_transfn))
+  {
+    dat <- dat %>% dplyr::mutate(dplyr::across(dplyr::ends_with("_dt"), .dt_transfn))
+  }
+
+  #compute age if bd_source is supplied
+  if (all(!is.null(bd_source), !is.null(bd_col_nm)))
+  {
+    dat <- dat %>% dplyr::mutate(age = compute_age(b_dt, start_dt))
+  }
 
 }
 
