57743ee364bf3182d8d00a9f1d24516a14dd87c4
Hu <kevin.hu@bccdc.ca>
2023-06-30 18:35:24 -0700

    identify_code() now works as intended in informal tests

--- R/age.R
+++ R/age.R
@@ -1,23 +1,33 @@
 compute_age <- function(bday, eday, trans = FALSE, .transfn = lubridate::ymd, verbose = TRUE) {
+  #compute age with lubridate functions (more accurate than /365.25) and built-in transformations
   if (trans) age <- lubridate::interval(.transfn(bday), .transfn(eday)) / lubridate::years(1)
   else age <- lubridate::interval(bday, eday) / lubridate::years(1)
+
+  #warnings for unreasonable values
   if (verbose)
   {
     if(is.na(min(age))) warning("Age contains missing value(s)")
     if(min(age, na.rm = TRUE) < 0) warning("Age contains negative value(s)")
     if(max(age, na.rm = TRUE) > 100) warning("Age contains value(s) > 100")
   }
+
   return(age)
 }
 
 cut_age <- function(age, lower_brks = c(0, 19, 25, 35, 45, 55), verbose = TRUE, ...) {
+  #create labels based on breaks
   labs <- sapply(1:length(lower_brks),
          function(i) dplyr::case_when(i == 1 ~ paste0("<", lower_brks[i+1]),
                                i == length(lower_brks) ~ paste0(lower_brks[i], "+"),
                                .default = paste(lower_brks[i], lower_brks[i+1]-1, sep = "-")))
+
+  #make age group with the lower boundary of breaks
   agegrp <- cut(age, breaks = c(lower_brks, Inf), labels = labs, right = FALSE, ...)
+
+  #warning if the breaks don't cover the full range
   if (verbose) {
     if (sum(is.na(agegrp)) > sum(is.na(age))) warning("More NA than raw age. Breaks do not cover the full range.")
   }
+
   return(agegrp)
 }
--- R/identify_code.R
+++ R/identify_code.R
@@ -1,5 +1,66 @@
-identify_code <- function(dat, source_nm, diag_cd_prefix, start_dt_nm, end_dt_nm = NULL, person_id_nm = "moh_study_id",
-                          codes, bd_source = NULL, bd_col_nm = NULL, .dt_transfn = NULL) {
+identify_code <- function(dat, clnt_id_nm = "moh_study_id", code_col_prefix, codes_vec, match_type = c("start", "exact", "regex"), n_per_clnt = 1, fuzzy_cd = NULL, collaspe_by_nm = NULL, trans_longer = FALSE) {
+  #stop if conflict
+  if(all(n_per_clnt == 1, !is.null(fuzzy_cd))) stop("Fuzzy code should not be supplied if only one record per client is required. If appropriate, include it in codes_vec instead.")
+
+  #add fuzzy_cd into code_vec so that only one filter is engouh afterward
+  codes_vec <- c(codes_vec, fuzzy_cd) %>% unique()
+
+  #use data.table and dtplyr to speed up the performance
+  dat <- dtplyr::lazy_dt(dat)
+
+  #pivot longer to simplify logic of interpreting n per client and filtering
+  if (trans_longer) {
+    dat <- dat %>%
+      tidyr::pivot_longer(
+        cols = dplyr::starts_with(code_col_prefix),
+        names_to = "cd_position",
+        names_prefix = code_col_prefix,
+        values_to = code_col_prefix,
+        values_drop_na = TRUE
+      ) #%>%
+    #dplyr::as_tibble()
+  }
+
+  #code filter by match types
+  if (match_type == "start")
+  {
+    dat <- dplyr::filter(dat, stringr::str_detect(.data[[code_col_prefix]], paste("^", codes_vec, collapse = "|", sep = "")))
+  }
+  if (match_type == "regex")
+  {
+    dat <- dplyr::filter(dat, stringr::str_detect(.data[[code_col_prefix]], paste(codes_vec, collapse = "|", sep = "")))
+  }
+  if (match_type == "exact")
+  {
+    dat <- dplyr::filter(dat, .data[[code_col_prefix]] %in% codes_vec)
+  }
+
+  #save intermediate data.table result
+  dat <- as_tibble(dat)
+
+  #job done if getting any records
+  if (n_per_clnt == 1) return(dat)
+  #keep records for person with number of records >= n_per_clnt
+  else
+  {
+    dat <- dat %>%
+      dplyr::group_by(.data[[clnt_id_nm]])
+
+    #count differently if unit id is supplied by collaspe_by_nm
+    if (!is.null(collaspe_by_nm)) dat <- dat %>% dplyr::filter(dplyr::n_distinct(.data[[collaspe_by_nm]]) >= n_per_clnt)
+    else dat <- dat %>% dplyr::filter(dplyr::n() >= n_per_clnt)
+
+    #records cannot be all fuzzy within person
+    if (!is.null(fuzzy_cd)) dat <- dat %>% dplyr::filter(!all(.data[[code_col_prefix]] %in% fuzzy_cd))
+
+    #clean up grouping before output
+    dat %>% dplyr::ungroup()
+
+  }
+
+}
+
+compare_dates <- function(dat, source_nm, code_col_prefix, code, start_dt_nm, end_dt_nm = NULL, person_id_nm = "moh_study_id", bd_source = NULL, bd_col_nm = NULL, .dt_transfn = NULL) {
 
   #get age
   if (!is.null(bd_source))
@@ -20,7 +81,7 @@ identify_code <- function(dat, source_nm, diag_cd_prefix, start_dt_nm, end_dt_nm
     )) %>%
     #remove empty diag columns
     dplyr::select(dplyr::where(~!all(is.na(.)))) %>%
-    dplyr::rename_with(.fn = ~ gsub(diag_cd_prefix, "diag_cd", .), .col = dplyr::starts_with(diag_cd_prefix)) %>%
+    dplyr::rename_with(.fn = ~ gsub(code_col_prefix, "diag_cd", .), .col = dplyr::starts_with(code_col_prefix)) %>%
     dplyr::mutate(source = source_nm)
 
   #transform dates if needed
@@ -41,3 +102,5 @@ identify_code <- function(dat, source_nm, diag_cd_prefix, start_dt_nm, end_dt_nm
 # toy_dat <- data.frame(msp_id = 1:10, moh_study_id = sample(1000:9999, size = 10), start_dt_nm = sample(seq(ymd("2020-01-01"), ymd("2023-12-31"), by = 1), size = 10), diagx = sample(1000:9999, size = 10), diagx_1 = sample(1000:9999, size = 10), diagx_2 = NA, din_pin = sample(1000:9999, size = 10))
 
 # toy_dat_t <- identify_code(toy_dat, source_nm = "msp", start_dt_nm = "start_dt_nm", end_dt_nm = NULL, diag_cd_prefix = "din_pin")
+
+# toy_dat <- data.frame(moh_study_id = c(rep(1,4), rep(2,4)), cd_1 = c("304", "305", "50B", "123", rep("234", 4)), cd_2 = c(rep(NA, 4), "304", rep(NA,3)), date = c(rep("2020-01-01",2), "2020-01-02", "2020-01-03", paste0("2020-01-0", 1:4)))
