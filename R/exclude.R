8d0966add365ea4baab49b2a2e36cff165a6243a
Hu <kevin.hu@bccdc.ca>
2023-07-27 08:49:08 -0700

    check point before pushing to github

--- R/exclude.R
+++ R/exclude.R
@@ -1,24 +1,19 @@
 exclude <- function(data, excl = NULL, by = NULL, condition = NULL, verbose = TRUE, report_on_nm = NULL, ...) {
   if (is.null(excl)) {
-
-    if (is.null(substitute(condition))) stop("The `condition` argument must be supplied if `excl` is absent")
-    else {
-      #direct filter
-      #drop_rows <- do.call(dplyr::filter, list(incl, rlang::parse_expr(excl)))
-      keep_rows <- dplyr::filter(data, !({{condition}}), ...)
+    #if only data is supplied, do filter with negation
+    if (is.null(substitute(condition))) {
+      stop("The `condition` argument must be supplied if `excl` is absent")
+    } else {
+      keep_rows <- dplyr::filter(data, !({{ condition }}), ...)
       if (verbose) cat("\nExclude a subset of `data` that satisfies condition:", deparse(substitute(condition)), "\n")
-      #return(keep_rows)
     }
-
-  }
-
-  else if (any(class(data) %in% class(excl))) {
-    keep_rows <- dplyr::anti_join(data, excl, by = {{by}}, ...)
+  } else if (any(class(data) %in% class(excl))) {
+    #if two data sets, do anti_join
+    keep_rows <- dplyr::anti_join(data, excl, by = {{ by }}, ...)
     if (verbose) cat("\nExclude records in `data` through anti_join with `excl` matching on (by argument):", deparse(substitute(by)), "\n")
-
+  } else {
+    stop("Both incl and excl must be remote tables or local data frames. Try collect() the remote table - may be slow if collecting large data - before runing the function.")
   }
-
-  else stop("Both incl and excl must be remote tables or local data frames. Try collect() the remote table - may be slow if collecting large data - before runing the function.")
 
   if (!is.null(report_on_nm)) {
     cat_n <- report_n(keep_rows, data, on_nm = report_on_nm)
--- R/report_n.R
+++ R/report_n.R
@@ -10,24 +10,22 @@
 #' @export
 #'
 #' @examples
-#' #some exclusions
+#' # some exclusions
 #' iris_1 <- subset(iris, Petal.Length > 1)
 #' iris_2 <- subset(iris, Petal.Length > 2)
 #'
-#' #get n at each operation
+#' # get n at each operation
 #' n <- report_n(iris, iris_1, iris_2, on_nm = "Species")
 #' n
 #'
-#' #get the difference at each step
+#' # get the difference at each step
 #' diff(n)
 report_n <- function(..., on_nm) {
   if (!is.character(on_nm)) stop("The `on_nm` argument must be character.")
 
   if (all(sapply(list(...), function(x) on_nm %in% names(x)))) {
     sapply(list(...), function(x) dplyr::group_by(x, .data[[on_nm]]) %>% dplyr::n_groups())
+  } else {
+    stop("All data must have the", on_nm, "column.")
   }
-
-  else stop("All data must have the", on_nm, "column.")
 }
-
-
