677b203300ddbb9110b2262e055845966c9bf90b
Hu <kevin.hu@bccdc.ca>
2023-08-02 11:40:34 -0700

    improved report_n to accept unquoted on argument

--- R/report_n.R
+++ R/report_n.R
@@ -4,9 +4,9 @@
 #' This function is intended to mimic dplyr::n_distinct() for multiple inputs. It is useful to report the number of clients through out a series of inclusion or exclusion. An use case could be getting the Ns for the sample definition flowchart in an epidemiological study.
 #'
 #' @param ... Data frames or remote tables (e.g., from dbplyr)
-#' @param on_nm The column name to report on. It must be present in all data sources.
+#' @param on The column to report on. It must be present in all data sources.
 #'
-#' @return A sequence of distinct n in on_nm for each data frames
+#' @return A sequence of the number of distinct `on` for each data frames
 #' @export
 #'
 #' @examples
@@ -15,17 +15,17 @@
 #' iris_2 <- subset(iris, Petal.Length > 2)
 #'
 #' # get n at each operation
-#' n <- report_n(iris, iris_1, iris_2, on_nm = "Species")
+#' n <- report_n(iris, iris_1, iris_2, on = Species)
 #' n
 #'
 #' # get the difference at each step
 #' diff(n)
-report_n <- function(..., on_nm) {
-  if (!is.character(on_nm)) stop("The `on_nm` argument must be character.")
+report_n <- function(..., on) {
+  on <- rlang::as_name(enquo(on))
 
   col_nm <- sapply(list(...), function(x) {if ("dtplyr_step" %in% class(x)) x[["vars"]] else names(x)})
 
-  if (all(sapply(col_nm, function(x) on_nm %in% x))) stop("All data must have the ", on_nm, " column.")
+  if (all(sapply(col_nm, function(x) on %in% x))) stop("All data must have the ", on, " column.")
 
-  sapply(list(...), function(x) dplyr::group_by(x, .data[[on_nm]]) %>% dplyr::n_groups())
+  sapply(list(...), function(x) dplyr::group_by(x, .data[[on]]) %>% dplyr::n_groups())
 }
