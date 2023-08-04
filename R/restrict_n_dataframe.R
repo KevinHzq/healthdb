dbd9e3a82499ab30ad639a97a777b7559fdd2326
Hu <kevin.hu@bccdc.ca>
2023-08-04 16:05:46 -0700

    documented restrict_n

--- R/restrict_n_dataframe.R
+++ R/restrict_n_dataframe.R
@@ -30,8 +30,8 @@ restrict_n.data.frame <- function(data, clnt_id = NULL, n_per_clnt, count_by = N
   dt <- dt[n_filter]
 
   if (verbose) {
-    initial_n <- report_n(data, on = clnt_id)
-    cat("\n Of the", initial_n, "clients in the input,", initial_n - report_n(dt, on = clnt_id), "were excluded by restricting that each client must have at least", n_per_clnt, "records", ifelse(has_count_by, paste0("with distinct ", count_by_nm), ""), "\n")
+    initial_n <- report_n(data, on = {{ clnt_id }})
+    cat("\n Of the", initial_n, "clients in the input,", initial_n - report_n(dt, on = {{ clnt_id }}), "were excluded by restricting that each client must have at least", n_per_clnt, "records", ifelse(has_count_by, paste0("with distinct ", count_by_nm), ""), "\n")
   }
 
   # convert back to dataframe before output
--- R/restrict_n_sql.R
+++ R/restrict_n_sql.R
@@ -38,8 +38,8 @@ restrict_n.tbl_sql <- function(data, clnt_id = NULL, n_per_clnt, count_by = NULL
   }
 
   if (verbose) {
-    initial_n <- report_n(data, on = clnt_id)
-    cat("\n Of the", initial_n, "clients in the input,", initial_n - report_n(db, on = clnt_id), "were excluded by restricting that each client must have at least", n_per_clnt, "records", ifelse(has_count_by, paste0("with distinct ", count_by_nm), ""), "\n")
+    initial_n <- report_n(data, on = {{ clnt_id }})
+    cat("\n Of the", initial_n, "clients in the input,", initial_n - report_n(db, on = {{ clnt_id }}), "were excluded by restricting that each client must have at least", n_per_clnt, "records", ifelse(has_count_by, paste0("with distinct ", count_by_nm), ""), "\n")
   }
 
   return(db)
