aadd20756af9b2b9ba5de5e432cc18fd109af62d
Hu <kevin.hu@bccdc.ca>
2023-08-08 10:56:24 -0700

    documented exclude and removed dtplyr conversion from it due to error when using join_by

--- R/exclude.R
+++ R/exclude.R
@@ -1,16 +1,36 @@
+#' Remove rows based on conditions or another data set
+#'
+#' @description
+#' This function combines `dplyr::anti_join` and the opposite of `dplyr::filter`. When the second data set is supplied through the `excl` argument, anti join would be performed; otherwise, `data` would be filtered with negation of the expression given via the `condition` argument.
+#'
+#' @param data Data frames or remote tables (e.g., from dbplyr). A subset will be removed from this data.
+#' @param excl Data frames or remote tables (e.g., from dbplyr). Rows/values present in it will be removed from `data` if there is a match. This will be passed to dplyr::anti_join as the second argument.
+#' @param by Column names that should be matched by anti_join or dplyr::join_by expressions. See anti_join's `by` argument for detail. Default NULL is the same as setdiff(data, excl).
+#' @param condition An expression that will be negated then passed to dplyr::filter, so the rows that satisfy `condition` are those to be removed from `data`.
+#' @param verbose A logical for whether printing explanation for the operation.
+#' @param report_on A quoted/unquoted column name for counting how many of its distinct values were removed from `data`, e.g., counting how many client IDs were removed. Default is NULL.
+#' @param ... Additional arguments passing to filter/anti_join for finer control of matching, e.g., na action, by-group filtering, etc.
+#'
+#' @return A data frame or remote table that is a subset of `data`.
+#' @export
+#'
+#' @examples
+#' #exclude with condition
+#' cyl4 <- exclude(mtcars, condition = cyl == 4, report_on = cyl)
+#'
+#' #exclude with another data
+#' exclude(mtcars, cyl4, dplyr::join_by(cyl), report_on = cyl)
 exclude <- function(data, excl = NULL, by = NULL, condition = NULL, verbose = TRUE, report_on = NULL, ...) {
   if (is.null(excl)) {
     #if only data is supplied, do filter with negation
     if (is.null(substitute(condition))) {
       stop("The `condition` argument must be supplied if `excl` is absent")
     } else {
-      if ("data.frame" %in% class(data)) data <- dtplyr::lazy_dt(data)
       keep_rows <- dplyr::filter(data, !({{ condition }}), ...)
       if (verbose) cat("\nExclude a subset of `data` that satisfies condition:", deparse(substitute(condition)), "\n")
     }
   } else if (any(class(data) %in% class(excl))) {
     #if two data sets, do anti_join
-    if ("data.frame" %in% class(data)) data <- dtplyr::lazy_dt(data)
     if (verbose) cat("\nExclude records in `data` through anti_join with `excl` matching on (by argument):", deparse(substitute(by)), "\n")
     keep_rows <- dplyr::anti_join(data, excl, by = {{ by }}, ...)
   } else {
@@ -22,6 +42,5 @@ exclude <- function(data, excl = NULL, by = NULL, condition = NULL, verbose = TR
     cat("\nOf the", cat_n[2], deparse(substitute(report_on)), "in data,", diff(cat_n), "were excluded.\n")
   }
 
-  if ("dtplyr_step" %in% class(keep_rows)) keep_rows <- dplyr::as_tibble(keep_rows)
   return(keep_rows)
 }
