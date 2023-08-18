a99731d11530852fd6c1ad11ec7b0abd1230a9a5
Hu <kevin.hu@bccdc.ca>
2023-08-18 16:58:35 -0700

    for build_def, fixed a bug related to unlisting fn_args and improved to allow unquoted function name input

--- R/build_def.R
+++ R/build_def.R
@@ -1,24 +1,30 @@
-build_def <- function(def_lab, src_labs, fn_labs, fn_args = NULL) {
-  #dots <- rlang::dots_list(..., .preserve_empty = TRUE)
-  #print(enquo(def_fn) %>% rlang::eval_tidy())
+build_def <- function(def_lab, src_labs, def_fn, fn_args) {
+  # capture names of the functions before any eval even before checking
+  fn_exprs <- rlang::try_fetch(rlang::enquo(def_fn) %>% rlang::call_args(),
+    error = function(cnd) {
+      rlang::abort("The 'def_fn' functions need to be unquoted and wrapped in list()", parent = cnd)
+    }
+  )
+  fn_labs <- purrr::map_chr(fn_exprs, rlang::as_name)
 
+  # input checking
   stopifnot(
-            rlang::is_character(def_lab),
-            #!rlang::is_empty(dots),
-            #rlang::is_function(def_fn),
-            rlang::is_named2(fn_args),
-            any(lengths(fn_args) %in% c(1, length(src_labs)))
-            )
+    rlang::is_character(def_lab),
+    rlang::is_character(src_labs),
+    rlang::is_list(fn_args),
+    purrr::map_lgl(def_fn, rlang::is_function),
+    rlang::is_named2(fn_args),
+    any(lengths(fn_args) %in% c(1, length(src_labs)))
+  )
 
-  #data <- rlang::enexprs(...)
-  #data <- purrr::map_chr(data_exprs, rlang::as_label)
-  #print(names(data))
+  # build separate tibbles with the arguments
+  # separate because later bind_cols can auto recycle shorter frames to match the longer ones
   df_data <- tidyr::expand_grid(def_lab, src_labs)
-  df_fn <- dplyr::tibble(`fn_labs` = fn_labs)
+  df_fn <- dplyr::tibble(`def_fn` = fn_labs)
   df_args <- dplyr::as_tibble(fn_args)
-  #print(df_args)
+  # print(df_args)
 
-  #get the arguments that are not supplied
+  # get the arguments that are not supplied (not needed anymore)
   # extra_args <- Filter(Negate(rlang::is_missing), rlang::fn_fmls(def_fn))
   # #extra_args <- rlang::call_args(rlang::call2(def_fn, !!!rlang::fn_fmls(def_fn)))
   # df_extra <- dplyr::as_tibble(map(extra_args, list)) %>%
@@ -26,12 +32,12 @@ build_def <- function(def_lab, src_labs, fn_labs, fn_args = NULL) {
   #
   # print(df_extra)
 
+  #build calls with fn and args leaving data empty for re-usability (e.g. supplying different with the same def via execute_def)
+  #unlist(recursive = FALSE) is necessary to prevent changing types of the variables in some cases
+  #i.e., n_per_clnt = c(2, 3) may end up as c(2, "3") when unlist
   df <- dplyr::bind_cols(df_data, df_fn, df_args) %>%
-    #dplyr::group_by(def_lab) %>%
     tidyr::nest(fn_args = dplyr::any_of(names(df_args))) %>%
-    dplyr::mutate(fn_call = purrr::map2(.data$fn_labs, .data$fn_args, function(x, y) rlang::call2(x, data = rlang::missing_arg(), rlang::splice(unlist(y)))))
-    # dplyr::mutate(new_fn = purrr::map2(fn_args, def_fn, function(x, y) rlang::new_function(rlang::dots_list(data = rlang::missing_arg(), rlang::splice(unlist(x)), rlang::splice(rlang::fn_fmls(y)), .preserve_empty = TRUE, .homonyms = "first"), body(y))))
-    #dplyr::mutate(src_fn = purrr::map2(fn_args, def_fn, function(x, y) rlang::new_function(rlang::dots_list(!!!rlang::fn_fmls(rlang::enquo(y)), rlang::splice(unlist(x)), .preserve_empty = TRUE, .homonyms = "last"), body(y))))
+    dplyr::mutate(fn_call = purrr::map2(.data[["def_fn"]], .data[["fn_args"]], function(x, y) rlang::call2(x, data = rlang::missing_arg(), rlang::splice(unlist(y, recursive = FALSE)))))
+
   return(df)
 }
-
