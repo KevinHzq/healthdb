b461fcf47880441016e52f78499b29ce72ab4edc
Hu <kevin.hu@bccdc.ca>
2023-08-25 16:06:57 -0700

    added force_proceed option to report_n

--- R/report_n.R
+++ R/report_n.R
@@ -5,6 +5,7 @@
 #'
 #' @param ... Data frames or remote tables (e.g., from dbplyr)
 #' @param on The column to report on. It must be present in all data sources.
+#' @param force_proceed A logical for whether to ask for user input in order to proceed when the data is not local data.frames, and a query needs to be executed before reporting. The default is TRUE to let user be aware of that the query execution may be slow. Use options(odcfun.force_proceed = FALSE) to suppress the prompt once and for all.
 #'
 #' @return A sequence of the number of distinct `on` for each data frames
 #' @export
@@ -20,7 +21,7 @@
 #'
 #' # get the difference at each step
 #' diff(n)
-report_n <- function(..., on) {
+report_n <- function(..., on, force_proceed = getOption("odcfun.force_proceed")) {
   rlang::check_required(on)
 
   on <- rlang::as_name(rlang::enquo(on))
@@ -29,5 +30,14 @@ report_n <- function(..., on) {
 
   if (all(sapply(col_nm, function(x) on %in% x))) stop("All data must have the ", on, " column.")
 
-  sapply(list(...), function(x) dplyr::group_by(x, .data[[on]]) %>% dplyr::n_groups())
+  sapply(list(...), function(x) {
+    #ask for user input if data is remote
+    if (!force_proceed & !is.data.frame(x)) {
+      proceed <- readline(prompt = "\nThe data is not a data.frame. The query has to be executed (may be slow) in order to be summarized. Proceed? [y/n]")
+
+      if (proceed == "n") stop("Try collect() the data first, or force_proceed = TRUE to slience the prompt.")
+    }
+
+    dplyr::group_by(x, .data[[on]]) %>% dplyr::n_groups()
+  })
 }
--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,8 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.odcfun <- list(
-    odcfun.verbose = TRUE
+    odcfun.verbose = TRUE,
+    odcfun.force_proceed = FALSE
   )
   toset <- !(names(op.odcfun) %in% names(op))
   if (any(toset)) options(op.odcfun[toset])
@@ -12,6 +13,13 @@
 
 #clean up
 .onUnload <- function(libpath) {
-  options(odcfun.verbose = NULL)
+  op <- options()
+  op.odcfun <- list(
+    odcfun.verbose = NULL,
+    odcfun.force_proceed = NULL
+  )
+  toremove <- names(op.odcfun) %in% names(op)
+  if (any(toremove)) options(op.odcfun[toremove])
+
   invisible()
 }
